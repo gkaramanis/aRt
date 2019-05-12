a375c139a1210a57c51fe4f05085b0e130efaaa4
Georgios Karamanis <>
2019-05-12 15:41:29 +0200

    everything is a function

--- squares.R
+++ squares.R
@@ -2,36 +2,29 @@ library(tidyverse)
 library(randomcoloR)
 
 makeMatrix <- function(n) {
-  z <- matrix(ncol = n + 1, nrow = n ^ 2)
+  z <- data.frame(matrix(ncol = n + 1, nrow = n ^ 2))
   
-  z[, 1] <- rep(1:n, rep(n, n), length.out = n ^ 2)
-  z[, 2] <- rep(1:n, rep(n, n))
+  z[1] <- rep(1:n, rep(n, n), length.out = n ^ 2)
+  z[2] <- rep(1:n, rep(n, n))
 
   for(i in 1:(n-1)) {
-    nam <- paste("V", i+2, sep = "")
-    z[, i+2] <- randomColor(count = n ^ 2)
-    # assign(nam, randomColor(count = n ^ 2))
+    z[i+2] <- randomColor(count = n ^ 2)
+    nam <- paste("X", i+2, sep = "")
+    q <- paste("+ geom_tile(aes(X1, X2, width = 0.8, height = 0.8, fill = ", nam, "))", sep = "")
   }
+  
+  print(q)
 
-  return(as.data.frame(z))
-  # data.frame(x, y, sapply(ls(pattern="^c[0-9]+$"), get))
+  ggplot(z) +
+    geom_tile(aes(X1, X2, fill = X3)) +
+    geom_tile(aes(X1, X2, width = 0.8, height = 0.8, fill = X4)) +
+    geom_tile(aes(X1, X2, width = 0.5, height = 0.5, fill = X5)) +
+    
+    theme_void() +
+    theme(legend.position = "none")
+  
 }
 
-m <- makeMatrix(4)
+makeMatrix(4)
 
-ggplot(m) +
-  geom_tile(aes(V1, V2,
-                fill = V3)) +
-  geom_tile(aes(V1, V2,
-                width = 0.8, height = 0.8,
-                fill = V4)) +
-  geom_tile(aes(V1, V2,
-                width = 0.5, height = 0.5,
-                fill = V5)) +
-  theme_void() +
-  theme(legend.position = "none")
-
-n=5
-z <- matrix(ncol = n + 1, nrow = n ^ 2)
-colnames(z[,1]) <- "x"
 
