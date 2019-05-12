edfd1672a6d5c7869a48b998b522fecc27a85630
Georgios Karamanis <>
2019-05-12 20:31:44 +0200

    it works!

--- squares.R
+++ squares.R
@@ -6,25 +6,18 @@ makeMatrix <- function(n) {
   
   z[1] <- rep(1:n, rep(n, n), length.out = n ^ 2)
   z[2] <- rep(1:n, rep(n, n))
-
+  
+  q <- "ggplot(z)"
   for(i in 1:(n-1)) {
     z[i+2] <- randomColor(count = n ^ 2)
     nam <- paste("X", i+2, sep = "")
-    q <- paste("+ geom_tile(aes(X1, X2, width = 0.8, height = 0.8, fill = ", nam, "))", sep = "")
+    wh <- 1-i/9
+    q <- paste(q, " + geom_tile(aes(X1, X2, width = ", wh, ", height = ", wh, ", fill = ", nam, "))", sep = "")
   }
   
-  print(q)
+  qq <- paste(q, "theme_void() + theme(legend.position = \'none\')", sep = " + ")
+  print(qq)
+  eval(parse(text = qq))
+  }
 
-  ggplot(z) +
-    geom_tile(aes(X1, X2, fill = X3)) +
-    geom_tile(aes(X1, X2, width = 0.8, height = 0.8, fill = X4)) +
-    geom_tile(aes(X1, X2, width = 0.5, height = 0.5, fill = X5)) +
-    
-    theme_void() +
-    theme(legend.position = "none")
-  
-}
-
-makeMatrix(4)
-
-
+makeMatrix(5)
