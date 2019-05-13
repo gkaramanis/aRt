0cf14e185b5606e61f7a9882aee21e8b56cbc523
Georgios Karamanis <georgios.karamanis@me.com>
2019-05-13 13:40:24 +0200

    changed code for intervals between squares

--- squares.R
+++ squares.R
@@ -11,11 +11,11 @@ makeMatrix <- function(n) {
   for(i in 1:(n-1)) {
     z[i+2] <- randomColor(count = n ^ 2)
     nam <- paste("X", i+2, sep = "")
-    wh <- 1-i/9
+    wh <- 1-(i-1)/n
     q <- paste(q, " + geom_tile(aes(X1, X2, width = ", wh, ", height = ", wh, ", fill = ", nam, "))", sep = "")
   }
   
-  qq <- paste(q, "theme_void() + theme(legend.position = \'none\')", sep = " + ")
+  qq <- paste(q, "theme_void() + theme(legend.position = \'none\') + ggsave(\"squares.png\", width = 2, height = 2)", sep = " + ")
   print(qq)
   eval(parse(text = qq))
   }
