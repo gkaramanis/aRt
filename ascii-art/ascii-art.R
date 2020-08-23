0624a11430c17bb61b3ff2a5e9ec57c3ca92b6f9
Georgios <georgios@Georgioss-MacBook-Pro.local>
2020-08-23 07:15:08 +0200

    Save text file

--- ascii-art/ascii-art.R
+++ ascii-art/ascii-art.R
@@ -1,12 +1,13 @@
 library(ggplot2)
 library(dplyr)
+library(stringr)
 library(magick)
 
-
+# Character ramp from http://paulbourke.net/dataformats/asciiart/
 ramp <- " .:-=+*#%@"
 
 # Read in image and convert to grayscale
-img_name <- "keanu"
+img_name <- "audrey"
 
 img <- image_read(paste0("ascii-art/images/", img_name, ".jpg")) %>%
   image_convert(colorspace = "gray")
@@ -36,14 +37,32 @@ img_df <- as.data.frame.table(img_array) %>%
 		# map b (0-255) to bf (1-0), so that "brighter" values become smaller numbers
 		bf = 1 - b / 255,
 		# and then map to i (1-10) to use with the character ramp
-		i = round(bf * 10)
+		i = round(bf * 10),
+		# replace 0 with 1
+		i = if_else(i == 0, 1, i)
 		) %>%
   rowwise() %>%
-  mutate(c = substr(ramp, i, i))
-		
+  mutate(c = substr(ramp, i, i)) %>% 
+  arrange(y, x)
+
+# Plot
 ggplot(img_df) +
+  # size of text is calculated from image height and is approximate
 	geom_text(aes(x, y, label = c), family = "Courier Bold", size = max(img_df$y)/20) +
 	scale_y_reverse() +
 	coord_fixed() +
 	theme_void() +
 	ggsave(paste0("ascii-art/plots/", img_name, "-ascii.png"), width = 8, height = 8 / img_ratio)
+
+# Find width of output
+w <- max(img_df$x)
+# Keep odd lines to prevent stretching
+txt_df <- img_df %>%
+	filter(y %% 2 == 1)
+
+# Write text file, insert \n after every row
+cat(
+  gsub(paste0("(.{", w, "})?"), "\\1\n", paste(txt_df$c, collapse = "")),
+file = paste0("ascii-art/plots/", img_name, "-ascii.txt")
+              )
+
